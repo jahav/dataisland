146d643caebd2e9a5003e5bec41c20b243d1c2dd
jahav <havlicek.honza@gmail.com>
2024-08-28 21:33:37 +0200

    Add info about exception

--- Sanctuary.xUnit/SanctuaryXunitExtensions.cs
+++ Sanctuary.xUnit/SanctuaryXunitExtensions.cs
@@ -25,3 +25,23 @@ public static class SanctuaryXunitExtensions
         keyValueStorage[$"{fixtureName}-factory"] = lake.Factory;
     }
 }
+
+/*
+TODO: Fix an exception
+Xunit.Sdk.TestPipelineException
+   Class fixture type 'Sanctuary.xUnit.ClassFixture' threw in its constructor
+      at Xunit.v3.FixtureMappingManager.GetFixture(Type fixtureType) in D:\a\xunit\xunit\src\xunit.v3.core\Utility\FixtureMappingManager.cs:line 184
+      at Xunit.v3.FixtureMappingManager.InitializeAsync(IReadOnlyCollection`1 fixtureTypes) in D:\a\xunit\xunit\src\xunit.v3.core\Utility\FixtureMappingManager.cs:line 226
+      at Xunit.v3.ExceptionAggregator.RunAsync(Func`1 code) in D:\a\xunit\xunit\src\xunit.v3.core\Exceptions\ExceptionAggregator.cs:line 120
+   
+   System.InvalidOperationException
+   Operations that change non-concurrent collections must have exclusive access. A concurrent update was performed on this collection and corrupted its state. The collection's state is no longer correct.
+      at System.Collections.Generic.Dictionary`2.TryInsert(TKey key, TValue value, InsertionBehavior behavior)
+      at System.Collections.Generic.Dictionary`2.set_Item(TKey key, TValue value)
+      at Sanctuary.SanctuaryXunitExtensions.AddSanctuary[TFixture](ServiceCollection services, ITenantLake lake) in C:\Users\havli\source\repos\sancturary\Sanctuary.xUnit\SanctuaryXunitExtensions.cs:line 24
+      at Sanctuary.xUnit.ClassFixture..ctor(TenantFixture tenantFixture) in C:\Users\havli\source\repos\sancturary\Sanctuary.xUnit\ClassFixture.cs:line 18
+      at System.RuntimeMethodHandle.InvokeMethod(Object target, Void** arguments, Signature sig, Boolean isConstructor)
+      at System.Reflection.MethodBaseInvoker.InvokeDirectByRefWithFewArgs(Object obj, Span`1 copyOfArgs, BindingFlags invokeAttr)
+   
+
+ */ 
