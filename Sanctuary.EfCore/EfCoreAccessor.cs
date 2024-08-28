760933c69333ccfb8ce2b369f6fc6356d72e4b94
jahav <havlicek.honza@gmail.com>
2024-08-28 22:49:33 +0200

    Shorten the name of a type parameter

--- Sanctuary.EfCore/EfCoreAccessor.cs
+++ Sanctuary.EfCore/EfCoreAccessor.cs
@@ -7,9 +7,10 @@ using Sanctuary.SqlServer;
 
 namespace Sanctuary.EfCore;
 
+/// <typeparam name="TDbContext">The concrete type of DbContext that is going to be instantiated.</typeparam>
 [PublicAPI]
-public class EfCoreAccessor<TDbContextImplementation> : IDataAccessorProvider<TDbContextImplementation>
-    where TDbContextImplementation : DbContext
+public class EfCoreAccessor<TDbContext> : IDataAccessorProvider<TDbContext>
+    where TDbContext : DbContext
 {
     public void Register(ServiceCollection serviceCollection)
     {
@@ -19,26 +20,26 @@ public class EfCoreAccessor<TDbContextImplementation> : IDataAccessorProvider<TD
 
         // Service must be scoped. DbContext is usually scoped, but in specific instances is singleton.
         var serviceDescriptors = serviceCollection
-            .Where(x => x.ServiceType == typeof(DbContextOptions<TDbContextImplementation>)).ToList();
+            .Where(x => x.ServiceType == typeof(DbContextOptions<TDbContext>)).ToList();
         if (serviceDescriptors.Count == 0)
         {
-            throw new InvalidOperationException($"Unable to find DbContextOptions<{typeof(TDbContextImplementation).Name}>. Ensure the {typeof(TDbContextImplementation).Name} is registered before calling this method.");
+            throw new InvalidOperationException($"Unable to find DbContextOptions<{typeof(TDbContext).Name}>. Ensure the {typeof(TDbContext).Name} is registered before calling this method.");
         }
 
         if (serviceDescriptors.Count > 1)
         {
-            throw new InvalidOperationException($"Found {serviceDescriptors.Count} registrations of DbContextOptions<{typeof(TDbContextImplementation).Name}>. Ensure it is registered exactly once.");
+            throw new InvalidOperationException($"Found {serviceDescriptors.Count} registrations of DbContextOptions<{typeof(TDbContext).Name}>. Ensure it is registered exactly once.");
         }
 
         var serviceDescriptor = serviceDescriptors.Single();
         if (serviceDescriptor.Lifetime == ServiceLifetime.Singleton)
         {
-            throw new InvalidOperationException($"DbContextOptions<{typeof(TDbContextImplementation).Name}> has singleton lifetime. It must have either scoped or transient lifetime.");
+            throw new InvalidOperationException($"DbContextOptions<{typeof(TDbContext).Name}> has singleton lifetime. It must have either scoped or transient lifetime.");
         }
 
         if (serviceDescriptor.ImplementationFactory is null)
         {
-            throw new InvalidOperationException($"DbContextOptions<{typeof(TDbContextImplementation).Name}> is not resolved through factory.");
+            throw new InvalidOperationException($"DbContextOptions<{typeof(TDbContext).Name}> is not resolved through factory.");
         }
 
         Func<IServiceProvider, object> originalFactory = serviceDescriptor.ImplementationFactory;
@@ -46,16 +47,16 @@ public class EfCoreAccessor<TDbContextImplementation> : IDataAccessorProvider<TD
         Func<IServiceProvider, object> newFactory = sp =>
         {
             var testContext = sp.GetRequiredService<ITestContext>();
-            var tenant = testContext.GetTenant<SqlDatabaseTenant>(typeof(TDbContextImplementation));
+            var tenant = testContext.GetTenant<SqlDatabaseTenant>(typeof(TDbContext));
 
-            var originalOptions = (DbContextOptions<TDbContextImplementation>)originalFactory(sp);
+            var originalOptions = (DbContextOptions<TDbContext>)originalFactory(sp);
             var relationalExtension = originalOptions.Extensions.OfType<RelationalOptionsExtension>().Single();
             var modifiedExtension = relationalExtension.WithConnectionString(tenant.ConnectionString);
             return originalOptions.WithExtension(modifiedExtension);
         };
 
         var newServiceDescriptor = new ServiceDescriptor(
-            typeof(DbContextOptions<TDbContextImplementation>),
+            typeof(DbContextOptions<TDbContext>),
             p => newFactory(p),
             serviceDescriptor.Lifetime); // Keep the original lifetime.
 
