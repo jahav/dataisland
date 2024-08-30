68725391cf23cf3629f6eb6724879dde1d261656
jahav <havlicek.honza@gmail.com>
2024-08-30 23:42:39 +0200

    Remove dead code

--- Sanctuary.Core/IComponentPool.cs
+++ Sanctuary.Core/IComponentPool.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Sanctuary;
 
 public interface IComponentPool<TComponent>
 {
-//    /// <summary>
-//    /// 
-//    /// </summary>
-//    /// <param name="componentName"></param>
-//    /// <returns></returns>
-//    /// <exception cref="InvalidOperationException">When the pool can't retrieve the component, generally due to exhausted resources.</exception>
-//    TComponent GetComponent(string componentName);
-
     IReadOnlyDictionary<string, TComponent> AcquireComponents(IReadOnlyDictionary<string, ComponentSpec> requestedComponents);
 }
