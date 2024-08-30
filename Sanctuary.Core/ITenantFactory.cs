92ce16ca7a6d60a1fa0a602d4921b750401252bd
jahav <havlicek.honza@gmail.com>
2024-08-30 23:51:14 +0200

    Rename file to match the class.

---
+++ Sanctuary.Core/ITenantFactory.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using System.Threading.Tasks;
+
+namespace Sanctuary;
+
+[PublicAPI]
+public interface ITenantFactory
+{
+    Task<object> AddTenantAsync(object component, string tenantName, object? dataSource);
+
+    Task RemoveTenantAsync(object component, object tenant);
+}
