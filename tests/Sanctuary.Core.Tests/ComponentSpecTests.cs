0d5a001e0ec18acb60644f08a896ddb1721af7e6
jahav <havlicek.honza@gmail.com>
2024-08-31 16:35:30 +0200

    Add tests to CI pipeline

---
+++ tests/Sanctuary.Core.Tests/ComponentSpecTests.cs
@@ -0,0 +1,15 @@
+namespace Sanctuary.Core.Tests;
+
+public class ComponentSpecTests
+{
+    [Fact]
+    public void ComponentType_property_returns_generic_type()
+    {
+        var componentSpec = new TestComponentSpec();
+        Assert.Equal(typeof(TestComponent), componentSpec.ComponentType);
+    }
+
+    private record TestComponentSpec : ComponentSpec<TestComponent>;
+
+    private class TestComponent;
+}
