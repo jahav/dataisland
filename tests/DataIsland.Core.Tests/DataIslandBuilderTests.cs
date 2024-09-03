30177475b7b12be2b2231421972feb84a29177a2
jahav <havlicek.honza@gmail.com>
2024-09-03 22:13:32 +0200

    Move tests for PatchServices correct place

--- tests/DataIsland.Core.Tests/DataIslandBuilderTests.cs
+++ tests/DataIsland.Core.Tests/DataIslandBuilderTests.cs
@@ -1,48 +1,10 @@
 ﻿using JetBrains.Annotations;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 namespace DataIsland.Core.Tests;
 
 public class DataIslandBuilderTests
 {
-    #region PatchServices
-
-    [Fact]
-    public void All_added_patches_are_applied_against_dependency_injection_container()
-    {
-        var patcher = new Mock<IDependencyPatcher<TestDataAccess>>();
-        var sut = new DataIslandBuilder()
-            .AddPatcher(patcher.Object)
-            .Build(Mock.Of<ITestContext>());
-
-        var services = new ServiceCollection();
-        sut.PatchServices(services);
-
-        patcher.Verify(x => x.Register(services), Times.Once);
-    }
-
-    [Fact]
-    public void One_patcher_can_patch_different_data_access_types()
-    {
-        var patcherAsInterface1 = new Mock<IDependencyPatcher<TestDataAccess>>();
-        var patcherAsInterface2 = patcherAsInterface1.As<IDependencyPatcher<TestDataAccess2>>();
-
-        Assert.Same(patcherAsInterface1.Object, patcherAsInterface2.Object);
-        var sut = new DataIslandBuilder()
-            .AddPatcher(patcherAsInterface1.Object)
-            .AddPatcher(patcherAsInterface2.Object)
-            .Build(Mock.Of<ITestContext>());
-
-        var services = new ServiceCollection();
-        sut.PatchServices(services);
-
-        patcherAsInterface1.Verify(x => x.Register(services), Times.Once);
-        patcherAsInterface2.Verify(x => x.Register(services), Times.Once);
-    }
-
-    #endregion
-
     #region AddPatcher
 
     [Fact]
---
+++ tests/DataIsland.Core.Tests/DataIslandTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using static DataIsland.Core.Tests.DataIslandBuilderTests;
+
+namespace DataIsland.Core.Tests;
+
+public class DataIslandTests
+{
+    #region PatchServices
+
+    [Fact]
+    public void All_added_patches_are_applied_against_dependency_injection_container()
+    {
+        var patcher = new Mock<IDependencyPatcher<TestDataAccess>>();
+        var sut = new DataIslandBuilder()
+            .AddPatcher(patcher.Object)
+            .Build(Mock.Of<ITestContext>());
+
+        var services = new ServiceCollection();
+        sut.PatchServices(services);
+
+        patcher.Verify(x => x.Register(services), Times.Once);
+    }
+
+    [Fact]
+    public void One_patcher_can_patch_different_data_access_types()
+    {
+        var patcherAsInterface1 = new Mock<IDependencyPatcher<TestDataAccess>>();
+        var patcherAsInterface2 = patcherAsInterface1.As<IDependencyPatcher<TestDataAccess2>>();
+
+        Assert.Same(patcherAsInterface1.Object, patcherAsInterface2.Object);
+        var sut = new DataIslandBuilder()
+            .AddPatcher(patcherAsInterface1.Object)
+            .AddPatcher(patcherAsInterface2.Object)
+            .Build(Mock.Of<ITestContext>());
+
+        var services = new ServiceCollection();
+        sut.PatchServices(services);
+
+        patcherAsInterface1.Verify(x => x.Register(services), Times.Once);
+        patcherAsInterface2.Verify(x => x.Register(services), Times.Once);
+    }
+
+    #endregion
+}
