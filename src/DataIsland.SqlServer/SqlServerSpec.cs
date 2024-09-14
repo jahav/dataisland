41f5b98276fce2acb0b574bfca4657733d252a45
jahav <havlicek.honza@gmail.com>
2024-09-14 18:10:25 +0200

    Add lined server spec property for SQL Server

--- src/DataIsland.SqlServer/SqlServerSpec.cs
+++ src/DataIsland.SqlServer/SqlServerSpec.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace DataIsland.SqlServer;
 
@@ -9,13 +10,15 @@ public sealed record SqlServerSpec : ComponentSpec<SqlServerComponent>
 
     internal bool? ClrEnabled { get; private set; }
 
+    internal HashSet<string> LinkedServerNames { get; private set; } = new();
+
     /// <summary>
     /// SQL Server must have a specified collation.
     /// </summary>
     /// <param name="collation">Desired collation name.</param>
     public SqlServerSpec WithCollation(string collation)
     {
-        return this with { Collation = collation };
+        return this with { Collation = collation, LinkedServerNames = [.. LinkedServerNames] };
     }
 
     /// <summary>
@@ -24,6 +27,18 @@ public sealed record SqlServerSpec : ComponentSpec<SqlServerComponent>
     /// <param name="clrEnabled">Desired setting of CLR.</param>
     public SqlServerSpec WithClrEnabled(bool clrEnabled)
     {
-        return this with { ClrEnabled = clrEnabled };
+        return this with { ClrEnabled = clrEnabled, LinkedServerNames = [.. LinkedServerNames] };
+    }
+
+    /// <summary>
+    /// SQL Server must have a linked server with a name <paramref name="name"/>. To
+    /// require multiple, call this method multiple times.
+    /// </summary>
+    /// <param name="name">Name of a linked server, i.e. the <c>@server</c> parameter
+    ///   of <c>sp_addlinkedserver</c>.</param>
+    public SqlServerSpec WithLinkedServerName(string name)
+    {
+        var linkedServers = new HashSet<string>(LinkedServerNames) { name };
+        return this with { LinkedServerNames = linkedServers };
     }
 }
---
+++ src/DataIsland.SqlServer/SysnameExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataIsland.SqlServer;
+
+internal static class SysnameExtensions
+{
+    private const string StartMaker = "[";
+    private const string EndMarker = "]";
+    private const string Separator = "], [";
+
+    public static string ToSysnameList(this IEnumerable<string> list)
+    {
+        return StartMaker + StartMaker + string.Join(Separator, list.Select(EscapeSysname)) + EndMarker;
+    }
+
+    public static string EscapeSysname(this string sysname)
+    {
+        // Sysname can't be null, it has automatic NOT NULL constraint
+        return sysname.Replace(StartMaker, "[[").Replace(EndMarker, "]]");
+    }
+}
