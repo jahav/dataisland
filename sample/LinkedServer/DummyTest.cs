ba35fdd6e006aa8f85fc3ccc939ff7aa2681f9d6
jahav <havlicek.honza@gmail.com>
2024-09-14 20:10:19 +0200

    Improve the LinkedList sample stub

--- sample/LinkedServer/DummyTest.cs
+++ sample/LinkedServer/DummyTest.cs
@@ -1,6 +1,27 @@
+using DataIsland.xUnit.v3;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace LinkedServer;
 
-public class DummyTest
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options);
+
+public class ClassFixture
+{
+    public ClassFixture(DataIslandFixture dataIslandFixture)
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer());
+
+        services.AddDataIslandInProc<ClassFixture>(dataIslandFixture.Island);
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    public ServiceProvider ServiceProvider { get; }
+}
+
+[ApplyTemplate("default")]
+public class DummyTest(ClassFixture _fixture) : IClassFixture<ClassFixture>
 {
     [Fact]
     public void Test_so_assembly_fixture_is_initialized()
